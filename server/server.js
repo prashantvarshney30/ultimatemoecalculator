42c9024588ff67614a5e255cfacde117e4ab9850
prashantvarshney30 <122198236+prashantvarshney30@users.noreply.github.com>
2023-01-08 15:13:01 +0530

    added port for render

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors')
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const barracktocapmap={
 	level1: "20",
